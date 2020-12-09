e236fbcecbdc73f181a5da5ab480e362b7e54957
ota-meshi <otameshiyo23@gmail.com>
2020-12-10 08:06:44 +0900

    remove useless code

--- tests/src/rules/no-parsing-error-in-vue-custom-block.ts
+++ tests/src/rules/no-parsing-error-in-vue-custom-block.ts
@@ -1,19 +1,12 @@
 import { RuleTester } from "eslint"
 import rule from "../../../src/rules/no-parsing-error-in-vue-custom-block"
-import { loadTestCases, makeSuiteTests } from "../../utils/utils"
+import { loadTestCases } from "../../utils/utils"
 
 const tester = new RuleTester({
     parser: require.resolve("yaml-eslint-parser"),
     parserOptions: {
         ecmaVersion: 2020,
     },
-})
-
-// TODO Delete when it are done.
-makeSuiteTests("no-parsing-error-in-vue-custom-block", {
-    default: [],
-    // never: ["never"],
-    // always: ["always"],
 })
 
 tester.run(
