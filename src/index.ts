b53c822ace806ef374d3d9f4b2ecfaf107a7a49a
yosuke ota <otameshiyo23@gmail.com>
2021-01-13 11:40:22 +0900

    Fixed missing config

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import type { RuleModule } from "./types"
 import { rules as ruleList } from "./utils/rules"
 import base from "./configs/base"
 import recommended from "./configs/recommended"
+import standard from "./configs/standard"
 
 const configs = {
     base,
     recommended,
+    standard,
 }
 
 const rules = ruleList.reduce((obj, r) => {
