8bd6364b02440c24c782fe4dd688a64e75771f50
yosuke ota <otameshiyo23@gmail.com>
2021-08-22 15:33:09 +0900

    Add support for eslint v8

--- tests/src/eslint-plugin.ts
+++ tests/src/eslint-plugin.ts
@@ -1,6 +1,6 @@
 import path from "path"
 import assert from "assert"
-import { CLIEngine } from "eslint"
+import { ESLint } from "./eslint-compat"
 import plugin from "../../src/index"
 
 // -----------------------------------------------------------------------------
@@ -10,26 +10,32 @@ import plugin from "../../src/index"
 const TEST_CWD = path.join(__dirname, "../fixtures/integrations/eslint-plugin")
 
 describe("Integration with eslint-plugin-yml", () => {
-    it("should lint without errors", () => {
-        const engine = new CLIEngine({
+    it("should lint without errors", async () => {
+        const engine = new ESLint({
             cwd: TEST_CWD,
             extensions: [".js", ".yml"],
+            plugins: { "eslint-plugin-yml": plugin },
         })
-        engine.addPlugin("eslint-plugin-yml", plugin)
-        const r = engine.executeOnFiles(["test01/src"])
-        assert.strictEqual(r.results.length, 2)
-        assert.strictEqual(r.errorCount, 0)
+        const results = await engine.lintFiles(["test01/src"])
+        assert.strictEqual(results.length, 2)
+        assert.strictEqual(
+            results.reduce((s, a) => s + a.errorCount, 0),
+            0,
+        )
     })
 
     // https://github.com/ota-meshi/eslint-plugin-yml/issues/89
-    it("should lint without error when use the 'no-multi-spaces' rule and block node together.", () => {
-        const engine = new CLIEngine({
+    it("should lint without error when use the 'no-multi-spaces' rule and block node together.", async () => {
+        const engine = new ESLint({
             cwd: TEST_CWD,
             extensions: [".yml"],
+            plugins: { "eslint-plugin-yml": plugin },
         })
-        engine.addPlugin("eslint-plugin-yml", plugin)
-        const r = engine.executeOnFiles(["issue89/src"])
-        assert.strictEqual(r.results.length, 1)
-        assert.strictEqual(r.errorCount, 0)
+        const results = await engine.lintFiles(["issue89/src"])
+        assert.strictEqual(results.length, 1)
+        assert.strictEqual(
+            results.reduce((s, a) => s + a.errorCount, 0),
+            0,
+        )
     })
 })
