2f55fc9ebd1a83715419167967a241fbc8ed26c1
yosuke ota <otameshiyo23@gmail.com>
2021-11-18 22:19:40 +0900

    Add `yml/no-multiple-empty-lines` rule

---
+++ tests/src/rules/no-multiple-empty-lines.ts
@@ -0,0 +1,16 @@
+import { RuleTester } from "eslint"
+import rule from "../../../src/rules/no-multiple-empty-lines"
+import { loadTestCases } from "../../utils/utils"
+
+const tester = new RuleTester({
+    parser: require.resolve("yaml-eslint-parser"),
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+})
+
+tester.run(
+    "no-multiple-empty-lines",
+    rule as any,
+    loadTestCases("no-multiple-empty-lines"),
+)
