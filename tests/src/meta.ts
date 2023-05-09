80663134b2aed60e793eeac2749ddba388f4b70d
ota-meshi <otameshiyo23@gmail.com>
2023-05-09 10:01:36 +0900

    feat: export meta object

---
+++ tests/src/meta.ts
@@ -0,0 +1,23 @@
+import assert from "assert";
+import plugin from "../../src";
+import { version } from "../../package.json";
+const expectedMeta = {
+  name: "eslint-plugin-yaml",
+  version,
+};
+
+describe("Test for meta object", () => {
+  it("A plugin should have a meta object.", () => {
+    assert.deepStrictEqual(plugin.meta, expectedMeta);
+  });
+
+  for (const [name, processor] of Object.entries(
+    // @ts-expect-error -- missing processors
+    plugin.processors || {}
+  )) {
+    it(`"${name}" processor should have a meta object.`, () => {
+      // @ts-expect-error -- missing type
+      assert.deepStrictEqual(processor.meta, expectedMeta);
+    });
+  }
+});
