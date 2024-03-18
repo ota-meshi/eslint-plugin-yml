350160be3835b2656a5dca6276e3637c42177d33
ota-meshi <otameshiyo23@gmail.com>
2024-03-19 07:03:58 +0900

    fix

--- src/meta.ts
+++ src/meta.ts
@@ -1,5 +1,5 @@
 // IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "npm run update"
-export const name = "eslint-plugin-yml" as const;
-export const version = "1.13.0" as const;
+export const name = "eslint-plugin-yml";
+export const version = "1.13.0";
